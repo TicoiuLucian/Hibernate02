b53f5687dcbb3739d3b78e6316fefe19c7591f6a
Ady <rusadrian2304@gmail.com>
2024-01-05 19:22:37 +0200

    Added 3 new caretaker's and 10 animals

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,13 +15,39 @@ public class Main {
     Animal animal3 = animalService.persist(new Animal("Pantera"));
 
     Caretaker caretaker = new Caretaker();
+    Caretaker caretaker1 = new Caretaker();
+    Caretaker caretaker2 = new Caretaker();
+    Caretaker caretaker3 = new Caretaker();
+
     caretaker.setAge(35);
     caretaker.setCaretakerName("Marcel");
     caretaker.addAnimal(new Animal("Lion"));
     caretaker.addAnimal(new Animal("Tiger"));
     caretaker.addAnimal(animal3);
 
+    caretaker1.setAge(40);
+    caretaker1.setCaretakerName("Ionut");
+    caretaker1.addAnimal(new Animal("Rhino"));
+    caretaker1.addAnimal(new Animal("Giraffe"));
+    caretaker1.addAnimal(new Animal("Boar"));
+
+    caretaker2.setAge(45);
+    caretaker2.setCaretakerName("Gheorghe");
+    caretaker2.addAnimal(new Animal("Deer"));
+    caretaker2.addAnimal(new Animal("Gazele"));
+    caretaker2.addAnimal(new Animal("Hiena"));
+
+    caretaker3.setAge(50);
+    caretaker3.setCaretakerName("Mihai");
+    caretaker3.addAnimal(new Animal("Dog"));
+    caretaker3.addAnimal(new Animal("Cat"));
+    caretaker3.addAnimal(new Animal("Ostrich"));
+
+
     caretakerService.persist(caretaker);
+    caretakerService.persist(caretaker1);
+    caretakerService.persist(caretaker2);
+    caretakerService.persist(caretaker3);
 
   }
 }
