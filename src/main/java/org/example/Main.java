8ede63b1ccc42e46ff241fb206915ee579007c20
GheldiuDanut <gheldiudanut98@yahoo.com>
2024-01-05 19:22:00 +0200

    Create new animals and caretakers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,13 +15,36 @@ public class Main {
     Animal animal3 = animalService.persist(new Animal("Pantera"));
 
     Caretaker caretaker = new Caretaker();
+    Caretaker caretaker2 = new Caretaker();
+    Caretaker caretaker3 = new Caretaker();
+
     caretaker.setAge(35);
+    caretaker2.setAge(32);
+    caretaker3.setAge(25);
+
     caretaker.setCaretakerName("Marcel");
+    caretaker2.setCaretakerName("Ion");
+    caretaker3.setCaretakerName("Vasi");
+
+
     caretaker.addAnimal(new Animal("Lion"));
     caretaker.addAnimal(new Animal("Tiger"));
+    caretaker.addAnimal(new Animal("Bear"));
+    caretaker.addAnimal(new Animal("Elephant"));
     caretaker.addAnimal(animal3);
 
+    caretaker2.addAnimal(new Animal("Tiger2"));
+    caretaker2.addAnimal(new Animal("Lion2"));
+    caretaker2.addAnimal(new Animal("Bear2"));
+
+    caretaker3.addAnimal(new Animal("Tiger3"));
+    caretaker3.addAnimal(new Animal("Lion3"));
+    caretaker3.addAnimal(new Animal("Bear3"));
+
+
     caretakerService.persist(caretaker);
+    caretakerService.persist(caretaker2);
+    caretakerService.persist(caretaker3);
 
   }
 }
